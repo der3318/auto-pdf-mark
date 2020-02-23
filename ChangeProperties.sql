b8d96b2cf53bd4c42ebfceca94c5aac79f1a1a0c
Der Chien <b03902015@ntu.edu.tw>
2020-02-23 15:02:06 +0800

    Delete ChangeProperties.sql

--- ChangeProperties.sql
+++
@@ -1,4 +0,0 @@
-UPDATE [trickster].[dbo].[tbl_item_option]
-SET [option_value] = 9999 /* desired value of properties */
-WHERE [item_uid] LIKE 4392015301 /* target item uid */
-AND [option_index] >= 10000 AND [option_index] <= 100010;
